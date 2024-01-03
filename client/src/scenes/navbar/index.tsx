6afe39408ec8d6d5279ee55ca0057ba6af603c94
Lakshan-K <lakshan67@hotmail.com>
2024-01-03 16:46:24 -0500

    Complete MUI configurations, themes, and React Router navigation

---
+++ client/src/scenes/navbar/index.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from "react";
+import FilterVintageIcon from "@mui/icons-material/FilterVintage";
+import { Link } from "react-router-dom";
+import { Box, Typography, useTheme } from "@mui/material";
+import FlexBetween from "@/components/FlexBetween";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+    const { palette } = useTheme();
+    const [selected, setSelected] = useState("dashboard");
+    return (
+        //LEFT SIDE
+        <FlexBetween mb="0.25rem" p="0.5rem 2rem" color={palette.grey[300]}>
+
+            <FlexBetween gap="0.75rem">
+                <FilterVintageIcon sx = {{fontSize: "28px"}}/>
+                <Typography variant="h4" fontSize="16px">
+                    CHOOSE NAME 
+                </Typography>
+            </FlexBetween>
+
+        {/*Right SIDE*/}
+            <FlexBetween gap="2rem">
+                <Box sx={{"&:hover": { color: palette.primary[100]}}}>
+                    <Link 
+                        to="/" 
+                        onClick={() => setSelected("dashboard")}
+                        style={{
+                            color: selected === "dashboard" ? "inherit" : palette.grey[700],
+                            textDecoration: "inherit"
+                        }}
+                        >
+                        Dashboard
+                    </Link>
+                </Box>
+
+                <Box sx={{"&:hover": { color: palette.primary[100]}}}>
+                    <Link 
+                        to="/predictions" 
+                        onClick={() => setSelected("predictions")}
+                        style={{
+                            color: selected === "predictions" ? "inherit" : palette.grey[700],
+                            textDecoration: "inherit"
+                        }}
+                        >
+                        Predictions
+                    </Link>
+                </Box>        
+            </FlexBetween>
+        </FlexBetween>
+    );
+};
+
+export default Navbar;
