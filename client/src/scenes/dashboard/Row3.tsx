02a915885dd0a6b7f5fe1ac8899ff2ccc495f651
Lakshan-K <lakshan67@hotmail.com>
2024-01-04 02:47:53 -0500

    Implement Dashboard Styling and Layout

---
+++ client/src/scenes/dashboard/Row3.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import DashboardBox from '@/components/DashboardBox'
+import React from 'react'
+
+type Props = {}
+
+function Row3({}: Props) {
+  return (
+    <>
+    <DashboardBox gridArea="g"></DashboardBox>
+    <DashboardBox gridArea="h"></DashboardBox>
+    <DashboardBox gridArea="i"></DashboardBox>
+    <DashboardBox gridArea="j"></DashboardBox>
+    </>
+  )
+}
+
+export default Row3
