54ff4aad7467944b46ffc5a0a299936745ffa0ab
Lakshan-K <lakshan67@hotmail.com>
2024-01-07 04:53:17 -0500

    Add Key Performance Indicators Route

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ import cors from "cors";
 import dotenv from "dotenv"
 import helmet from "helmet"
 import morgan from "morgan";
+import kpiRoutes from "./routes/kpi.js"
+import KPI from "./models/KPI.js";
+import { kpis } from "./data/data.js";
+
 
 /* CONFIGURATIONS */
 
@@ -18,7 +22,10 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors());
 
-console.log("hello")
+
+/*Routes*/
+app.use("/kpi", kpiRoutes);
+
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
@@ -30,10 +37,9 @@ mongoose
   .then(async () => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-    /* ADD DATA ONE TIME ONLY OR AS NEEDED */
+    //ADD DATA ONE TIME ONLY OR AS NEEDED
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis);
-    // Product.insertMany(products);
-    // Transaction.insertMany(transactions);
+
   })
   .catch((error) => console.log(`${error} did not connect`));
