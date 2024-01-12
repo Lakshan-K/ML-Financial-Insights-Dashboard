31d1bb7732a3894bc59e2a9c36c7b0b5f0cc11d0
Lakshan-K <lakshan67@hotmail.com>
2024-01-12 00:00:47 -0500

    Implement Products UI and API

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { GetKpisResponse } from "./types";
+import { GetKpisResponse, GetProductsResponse } from "./types";
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
@@ -22,4 +22,4 @@ export const api = createApi({
   });
 
 
-  export const { useGetKpisQuery } = api;
+  export const { useGetKpisQuery, useGetProductsQuery } = api;
