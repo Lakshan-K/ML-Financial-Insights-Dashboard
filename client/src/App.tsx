b1c80a4bdb07f8334ba612033c2a557586dec1e0
Lakshan-K <lakshan67@hotmail.com>
2024-01-14 17:11:39 -0500

    Implement Machine Learning Predictions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,31 @@
-import { ThemeProvider, createTheme } from "@mui/material/styles"
-import { useMemo } from "react"
-import { themeSettings } from "./theme"
-import { Box, CssBaseline } from "@mui/material"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Navbar from "@/scenes/navbar/index.tsx";
-import Dashboard from "@/scenes/dashboard/index.tsx";
+import { Box } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { themeSettings } from "./theme";
+import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
+import Predictions from "@/scenes/predictions";
 
 function App() {
-  const theme = useMemo(() => createTheme(themeSettings), [])
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
-      <div className="app">
-          <BrowserRouter>
-              <ThemeProvider theme={theme}>
-                  <CssBaseline />
-                  <Navbar /> {/* Add Navbar here */}
-                  <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
-                      <Routes>
-                          <Route path="/" element={<Dashboard />} />
-                          <Route path="/predictions" element={<div>predictions page</div>} />
-                      </Routes>
-                  </Box>
-              </ThemeProvider>
-          </BrowserRouter>
-      </div>
-  )
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<Predictions />} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
-export default App
 
-
+export default App;
